b877638731dde6a6d36f8a180b304f63da658c68
Rugile Vasaityte <rugile.vasaityte@gmail.com>
2024-03-01 18:47:12 +0200

    Refactoring

--- src/test/java/org/example/Testas4.java
+++ src/test/java/org/example/Testas4.java
@@ -1,35 +1,34 @@
 package org.example;
 
 import dev.failsafe.internal.util.Assert;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.time.Duration;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 public class Testas4 {
+    //private static String email_register = "user-438654255595719931@test.com";
     private static final String baseUrl = "https://demowebshop.tricentis.com/";
     private static String email = "";
     private static final String pass = "Pass123$";
     private static ChromeDriver driver;
-    @BeforeAll
-    public static void beforeAllCreateUser() {
+    @BeforeClass
+    public static void beforeClassCreateUser() {
         System.out.println("Before all");
         ChromeOptions options = new ChromeOptions();
-        //options.addArguments("--headless");
+        options.addArguments("--headless");
 
-        driver = new ChromeDriver();
+        driver = new ChromeDriver(options);
+        //driver = new ChromeDriver();
         driver.manage().window().maximize();
 
         email =  String.format("user%d%d@test.com", new Random().nextInt(), new Random().nextInt());
@@ -48,24 +47,34 @@ public class Testas4 {
         driver.quit();
     }
 
-    @AfterEach
+    @After
     public void afterEach() {
         if (driver != null) {
             driver.quit();
         }
     }
-    @BeforeEach
+
+    @Before
     public void beforeEach() {
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
+      ChromeOptions options = new ChromeOptions();
+      options.addArguments("--headless");
+      //driver = new ChromeDriver();
+      driver = new ChromeDriver(options);
+      driver.manage().window().maximize();
+      driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
     }
 
     private static void performScenario(String dataFile) {
         driver.get(baseUrl);
         driver.findElement(By.xpath("//a[@href='/login']")).click();
 
-        driver.findElement(By.id("Email")).sendKeys(email);
-        driver.findElement(By.id("Password")).sendKeys(pass);
+        WebElement emailElement = driver.findElement(By.id("Email"));
+        emailElement.click();
+        emailElement.sendKeys(email);
+
+        WebElement passwordElement = driver.findElement(By.id("Password"));
+        passwordElement.click();
+        passwordElement.sendKeys(pass);
 
         driver.findElement(By.cssSelector("input[value='Log in']")).click();
         driver.findElement(By.xpath("//a[@href='/digital-downloads']")).click();
@@ -74,7 +83,6 @@ public class Testas4 {
             List<String> lines = Files.readAllLines(Paths.get(dataFile));
             WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(30000));
             for (String line : lines) {
-                // System.out.println(line);
                 WebElement element = driver.findElement(By.xpath("//div[@class='item-box']//h2[@class='product-title']/a[text() = '" + line + "']/ancestor::div[@class='product-item']//input[@type='button'] "));
                 wait.until(ExpectedConditions.elementToBeClickable(element)).click();
                 Thread.sleep(500);
@@ -88,26 +96,22 @@ public class Testas4 {
         driver.findElement(By.id("checkout")).click();
 
         try {
-            // Attempt to find the element with id "billing-address-select"
             WebElement billingAddressSelect = driver.findElement(By.id("billing-address-select"));
-
-            // If the element is found, perform actions on it
             if (billingAddressSelect.isDisplayed()) {
                 System.out.println("Billing address select exists...");
-                // Perform actions on the billing address select element
                 billingAddressSelect.click();
                 billingAddressSelect.sendKeys("n");
                 billingAddressSelect.sendKeys(Keys.ENTER);
             }
         } catch (NoSuchElementException e) {
-            // Handle the case where the element does not exist
             System.out.println("Billing address select does not exist...");
         }
 
-        WebElement countryElement = driver.findElement(By.xpath("//*[@id='BillingNewAddress_CountryId']"));
-        countryElement.click();
+        WebElement countryElement = driver.findElement(By.id("BillingNewAddress_CountryId"));
+        countryElement.sendKeys(Keys.ENTER);
         countryElement.sendKeys("lit");
         countryElement.sendKeys(Keys.ENTER);
+
         driver.findElement(By.id("BillingNewAddress_City")).sendKeys("Vilnius");
         driver.findElement(By.id("BillingNewAddress_Address1")).sendKeys("Vilniaus g. 1");
         driver.findElement(By.id("BillingNewAddress_ZipPostalCode")).sendKeys("12345");
@@ -119,19 +123,23 @@ public class Testas4 {
         wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("input.button-1.payment-info-next-step-button"))).click();
         wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("input.button-1.confirm-order-next-step-button"))).click();
 
-        //WebElement element = driver.findElement(By.xpath("/html/body/div[4]/div[1]/div[4]/div/div/div[2]/div/div[1]/strong"));
-        // Assert that the element exists
-        //Assert.assertTrue(element.isDisplayed());
+        Assert.isTrue(elementExists(driver, "//strong[text()='Your order has been successfully processed!']"), "Order did not go through");
     }
 
+    public static boolean elementExists(WebDriver driver, String xpath) {
+        try {
+            driver.findElement(By.xpath(xpath));
+            return true;
+        } catch (org.openqa.selenium.NoSuchElementException e) {
+            return false;
+        }
+    }
     @Test
     public void testScenario1() {
         performScenario("data1.txt");
     }
-
     @Test
     public void testScenario2() {
         performScenario("data2.txt");
     }
-
 }
